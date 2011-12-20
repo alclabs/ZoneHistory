5a732666851bb5acd734be15c8ae96c798d8c2b1
sappling <sajakarta@appling.org>
2011-12-20 17:03:27 -0500

    more infrastructure for caching

--- src/main/java/com/controlj/addon/zonehistory/cache/ZoneHistory.java
+++ src/main/java/com/controlj/addon/zonehistory/cache/ZoneHistory.java
@@ -1,7 +1,19 @@
 package com.controlj.addon.zonehistory.cache;
 
+import com.controlj.green.addonsupport.access.Location;
+
 /**
  *
  */
 public class ZoneHistory {
+    private final Location equipmentColorLocation;
+
+    public ZoneHistory(Location equipmentColorLocation) {
+        this.equipmentColorLocation = equipmentColorLocation;
+    }
+
+    public Location getEquipmentColorLocation() {
+        return equipmentColorLocation;
+    }
+
 }
--- src/main/java/com/controlj/addon/zonehistory/cache/ZoneHistoryCache.java
+++ src/main/java/com/controlj/addon/zonehistory/cache/ZoneHistoryCache.java
@@ -1,7 +1,57 @@
 package com.controlj.addon.zonehistory.cache;
 
+import com.controlj.green.addonsupport.access.Location;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  */
-public class ZoneHistoryCache {
+public enum ZoneHistoryCache {
+    INSTANCE;
+
+    private Map<String, Collection<ZoneHistory>> ancestryMap = new HashMap<String, Collection<ZoneHistory>>();
+    private Map<String, ZoneHistory> zoneHistoryMap = new HashMap<String, ZoneHistory>();
+
+    public Collection<ZoneHistory> getDescendantZoneHistories(Location loc) {
+        String lus = loc.getTransientLookupString();
+
+        synchronized (this) {
+            return ancestryMap.get(lus);
+        }
+    }
+
+    public Collection<ZoneHistory> addDescendantZoneHistories(Location loc, Collection<ZoneHistory> newHistories) {
+        Collection<ZoneHistory> result;
+        String lus = loc.getTransientLookupString();
+        synchronized (this) {
+            Collection<ZoneHistory> existingHistories = ancestryMap.get(lus);
+            if (existingHistories == null) {
+                result = new ArrayList<ZoneHistory>();
+                for (ZoneHistory newHistory : newHistories) {
+                    result.add(getOfficialZoneHistory(newHistory));
+                }
+                ancestryMap.put(lus, result);
+                result = newHistories;
+            } else {
+                result = existingHistories;
+            }
+            return result;
+        }
+    }
+
+    private ZoneHistory getOfficialZoneHistory(ZoneHistory newHistory) {
+        String lus = newHistory.getEquipmentColorLocation().getTransientLookupString();
+        synchronized (this) {
+            ZoneHistory zoneHistory = zoneHistoryMap.get(lus);
+            if (zoneHistory == null) {
+                zoneHistoryMap.put(lus, newHistory);
+                zoneHistory = newHistory;
+            }
+            return zoneHistory;
+        }
+    }
 }
